d7f07d9cb685d22e7a01ea62f17d46e0335b54bd
arielbercovich <abercovich@udesa.edu.ar>
2023-11-08 17:01:12 -0300

    pequena correcion camera

--- my-app/src/components/Camara/MyCamera.js
+++ my-app/src/components/Camara/MyCamera.js
@@ -1,45 +1,69 @@
-import React, {Component} from "react";
-import {Camera} from 'expo-camera';
-import {db, storage} from '../../firebase/config';
-import {TouchableOpacity, View, Text} from 'react-native';
+import React, { Component } from "react";
+import { Camera } from 'expo-camera';
+import { View, Text, TouchableOpacity } from 'react-native';
 
-class MyCamera extends Component{
-constructor(props){
-super(props)
-this.state = {
-    permissions: false,
-    photo: '',// Url temporal interna de la foto 
-    showCamera: true, 
-}
-this.metodosDeCamera = '' // referenciar a los metodos internos del componente camera
-}
-componentDidMount(){
-    //pedir permiso para uso de hardware
-    Camera.requestCameraPermissionAsync()
-    .then(()=> {
-        this.setState(
-        {permissions: true})
-    .catch(e => console.log(e))
-        })
-}
+class MyCamera extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            permissions: false,
+            photo: null, // Cambiado a null para manejar la foto capturada
+        };
+        this.metodosDeCamera = null; // Cambiado a null para referenciar los métodos internos del componente camera
+    }
 
-render(){
-    return(
-        <View>
-            <Camera 
-            type={Camera.Constants.Type.front}
-            ref={ metodosDeCamera=> this.metodosDeCamera = metodosDeCamera}
-            />
-            <TouchableOpacity>
+    componentDidMount() {
+        // Pedir permiso para uso de hardware
+        Camera.requestCameraPermissionsAsync()
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    this.setState({ permissions: true });
+                } else {
+                    console.log("No se otorgaron los permisos de la cámara.");
+                }
+            })
+            .catch((e) => console.log(e));
+    }
 
-            <Text>
-                Sacar foto
-            </Text>
-            </TouchableOpacity>
-        </View>
-        )
-    
-}
+    takePicture = async () => {
+        if (this.metodosDeCamera) {
+            try {
+                const photo = await this.metodosDeCamera.takePictureAsync();
+                this.setState({ photo });
+            } catch (error) {
+                console.error("Error al tomar la foto:", error);
+            }
+        }
+    };
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                {this.state.permissions ? (
+                    this.state.photo ? ( // Si se ha capturado una foto, muestra la foto
+                        <View style={{ flex: 1 }}>
+                            <Text>Foto capturada:</Text>
+                            <Image source={{ uri: this.state.photo.uri }} style={{ flex: 1 }} />
+                        </View>
+                    ) : ( // Si no se ha capturado una foto, muestra la cámara
+                        <Camera
+                            type={Camera.Constants.Type.front}
+                            ref={(metodosDeCamera) => (this.metodosDeCamera = metodosDeCamera)}
+                            style={{ flex: 1 }}
+                        />
+                    )
+                ) : (
+                    <Text>No se otorgaron permisos de cámara.</Text>
+                )}
+
+                {this.state.photo ? null : (
+                    <TouchableOpacity onPress={this.takePicture}>
+                        <Text>Sacar foto</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+        );
+    }
 }
 
 export default MyCamera;
