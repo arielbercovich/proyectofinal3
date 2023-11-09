3fde90c2e50bd6706f44da6b5e7270e898108732
arielbercovich <abercovich@udesa.edu.ar>
2023-11-09 11:48:38 -0300

    intento de profile, fallido

--- my-app/App.js
+++ my-app/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
